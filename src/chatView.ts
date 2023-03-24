453972a8fca0e7a4a64086f088a698c710ce4a6b
Ben Force <benforce@gmail.com>
2023-03-24 16:16:02 -0500

    feat: display webview

---
+++ src/chatView.ts
@@ -0,0 +1,62 @@
+import * as vscode from 'vscode';
+
+// Reference: https://code.visualstudio.com/api/extension-guides/webview
+// Example: https://github.com/microsoft/vscode-extension-samples/blob/main/webview-view-sample/src/extension.ts
+
+export class ChatView implements vscode.WebviewViewProvider {
+
+  public static readonly viewType = 'sideai.chatView';
+
+  private _view?: vscode.WebviewView;
+
+  constructor(private readonly _extensionUri: vscode.Uri) { }
+
+  resolveWebviewView(webviewView: vscode.WebviewView, context: vscode.WebviewViewResolveContext<unknown>, token: vscode.CancellationToken): void | Thenable<void> {
+    this._view = webviewView;
+
+    webviewView.webview.options = {
+      // Allow scripts in the webview
+      enableScripts: true,
+
+      localResourceRoots: [
+        this._extensionUri
+      ]
+    };
+
+    webviewView.webview.html = this._getHtmlForWebview(webviewView.webview);
+
+    webviewView.webview.onDidReceiveMessage(data => {
+      console.info(data);
+    });
+  }
+  private _getHtmlForWebview(webview: vscode.Webview): string {
+    const nonce = getNonce();
+
+    return `<!DOCTYPE html>
+			<html lang="en">
+			<head>
+				<meta charset="UTF-8">
+				<!--
+					Use a content security policy to only allow loading styles from our extension directory,
+					and only allow scripts that have a specific nonce.
+					(See the 'webview-sample' extension sample for img-src content security policy examples)
+				-->
+				<meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource}; script-src 'nonce-${nonce}';">
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+				<title>Side AI</title>
+			</head>
+			<body>
+				<h1>Side AI</h1>
+			</body>
+			</html>`;
+  }
+}
+
+function getNonce() {
+  let text = '';
+  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < 32; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+}
