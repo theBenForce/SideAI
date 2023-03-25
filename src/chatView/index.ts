6234a8186eb72913c355f9d209090d63009fca14
Ben Force <benforce@gmail.com>
2023-03-24 21:58:48 -0500

    feat: add tailwind

--- src/chatView/index.ts
+++ src/chatView/index.ts
@@ -29,10 +29,18 @@ export class ChatView implements vscode.WebviewViewProvider {
       console.info(data);
     });
   }
+
+
   private _getHtmlForWebview(webview: vscode.Webview): string {
     const nonce = getNonce();
 
-    return `<!DOCTYPE html>
+
+    const chatViewUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'chatView.js'));
+    const tailwindCssUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'tailwindcss.js'));
+    const daisyUiUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'styles', 'daisyui.css'));
+    const showdownUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'node_modules', 'showdown', 'dist', 'showdown.min.js'));
+
+    return `<!DOCTYPE html data-theme="cupcake">
 			<html lang="en">
 			<head>
 				<meta charset="UTF-8">
@@ -43,10 +51,25 @@ export class ChatView implements vscode.WebviewViewProvider {
 				-->
 				<meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource}; script-src 'nonce-${nonce}';">
 				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script src="${showdownUri}" nonce="${nonce}"></script>
+
+        <link rel="stylesheet" href="${daisyUiUri}" nonce="${nonce}" type="text/css" />
+        <script src="${tailwindCssUri}" nonce="${nonce}"></script>
 				<title>Side AI</title>
 			</head>
 			<body>
-				<h1>Side AI</h1>
+        <div class="flex flex-col h-full v-full">
+          <div class="flex flex-col grow">
+            <div class="chat chat-start">
+              <div class="chat-bubble">It's over Anakin, <br/>I have the high ground.</div>
+            </div>
+          </div>
+          <div class="flex flex-row">
+          </div>
+        </div>
+
+        
+				<script src="${chatViewUri}" nonce="${nonce}"></script>
 			</body>
 			</html>`;
   }
